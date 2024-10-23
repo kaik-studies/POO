12e971cbd5e328a67cdcf9ee8bf1482d2f6f1042
kaik-studies <164260383+kaik-studies@users.noreply.github.com>
2024-10-23 19:57:47 -0300

    Create TESTE_02.java

---
+++ TESTE_02.java
@@ -0,0 +1,13 @@
+package TESTE_01;
+public class TESTE_01 {
+public static void main(String[] args) {
+
+byte b_positivo_maximo = 127;
+byte b_negativo_minimo = -128;
+b_positivo_maximo++;
+short s = 10000;
+int i = 2000000;
+long l = 2000000000;
+System.out.println(b_positivo_maximo);
+}
+}
