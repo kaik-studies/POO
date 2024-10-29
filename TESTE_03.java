3bd527166498a986f3e202fa27f242fbb6b1709c
kaik-studies <164260383+kaik-studies@users.noreply.github.com>
2024-10-29 19:45:52 -0300

    Create TESTE_03.java

---
+++ TESTE_03.java
@@ -0,0 +1,18 @@
+package projeto_aprende_java;
+
+public class aula_02 {
+
+public static void main(String[] args) {
+	
+	String abu = "Limao";
+	String aba = "damiel so o mi";
+	System.out.println(aba);
+	System.out.println(abu);
+	System.out.println(abu + aba);
+	char pipoca = 'A';
+	char pitomba = 'B';
+	System.out.println(pipoca + pitomba);
+}
+}
+
+//referente à aula do dia 29 de outubro de 2024, terca-feira, segunda atividade
