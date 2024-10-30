f09ff406bc7ddb722191edebd8b606fe7fb85634
kaik-studies <164260383+kaik-studies@users.noreply.github.com>
2024-10-30 19:38:09 -0300

    Create TESTE_04.java

---
+++ TESTE_04.java
@@ -0,0 +1,29 @@
+package projeto_aprende_java;
+
+public class aula_02 {
+ {
+	
+	float abu = 34.5f;
+	System.out.println(abu*2);
+}
+public static void main(String[] args) {
+	
+	String nome = "Daniel";
+	String sobrenome = "da Silva";
+	int nunDeAnimais = 2;
+	String animal = "gato(s)";
+	
+	System.out.printf("%s %s tem %d %s. %n", nome, sobrenome, nunDeAnimais, animal);
+	
+	String name = "Maria";
+	int age = 17;
+	float height = 1.65f;
+	
+	System.out.printf("%s tem %d anos de idade. Também tem %.2f de altura.%n", name, age, height);
+	System.out.println(name + " tem " + age + " anos de idade." + " " + "Também tem " + height + " de altura.");
+}
+}
+
+/* referente à aula do dia 30 de outubro de 2024. 
+Terceira atividade.
+*/
