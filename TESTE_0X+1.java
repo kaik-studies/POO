8eb7ab618cedc77b66c7ad2c6cb95693f97d68e6
kaik-studies <164260383+kaik-studies@users.noreply.github.com>
2024-11-05 19:28:51 -0300

    Create TESTE_0X+1.java

---
+++ TESTE_0X+1.java
@@ -0,0 +1,13 @@
+package projeto_aprende_java;
+
+import java.util.Scanner;
+
+public class aula_02 {
+	public static void main(String[] args) {
+		Scanner myObj = new Scanner(System.in);
+		String userName;
+		 System.out.println("Enter Username ");
+		 userName = myObj.nextLine();
+		 System.out.println("Username is: " + userName);
+	}
+}
