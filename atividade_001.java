92511a4fcb8832d52ec6851de275d01ef96f6d56
kaik-studies <164260383+kaik-studies@users.noreply.github.com>
2024-11-06 15:20:39 -0300

    Update atividade_001.java

--- atividade_001.java
+++ atividade_001.java
@@ -79,4 +79,74 @@ public class atividade_001 {
 
 //essa resolução é melhor
 
+package atividadeUm;
+import java.util.Scanner;
+public class atividade_001 {
+	public static void main(String[] args) {
+		Scanner myObj = new Scanner(System.in);
+		int X;
+		int Y;
+		int Z;
+		 System.out.println("Digite sua idade.");
+			X = myObj.nextInt();
+			 System.out.println("Digite a idade de um amigo.");
+				Y = myObj.nextInt();
+		 if (X > Y) {
+			 System.out.println("Você é mais velho que seu amigo.");
+		 }
+			 else if(X == Y) {
+				 System.out.println("Você é mais velho que seu amigo.");
+			 }
+			 else {
+				 System.out.println("Você é mais novo que seu amigo.");
+			 }
+		 }
+}
 
+//quarta questão
+
+package atividadeUm;
+import java.util.Scanner;
+public class atividade_001 {
+	public static void main(String[] args) {
+		Scanner myObj = new Scanner(System.in);
+		int X;
+		int Y;
+		int Z;
+		 System.out.println("Digite um valor.");
+			X = myObj.nextInt();
+/*			 System.out.println("Digite a idade de um amigo.");
+				Y = myObj.nextInt(); 
+				*/
+		 if (X > 20) {
+			 System.out.println("Seu valor excede 20.");
+		 }
+			 else if(X <= 20 && X >= 10) {
+				 System.out.println("Seu valor é aceitável.");
+			 }
+			 else {
+				 System.out.println("Seu valor é inferior à 10.");
+			 }
+		 }
+}
+
+//quinta questão
+
+package atividadeUm;
+import java.util.Scanner;
+public class atividade_001 {
+	public static void main(String[] args) {
+		Scanner myObj = new Scanner(System.in);
+		float X;
+		float Y;
+		 System.out.println("Digite um preço de produto.");
+			X = myObj.nextFloat();			 
+			System.out.println("Digite o valor de desconto.");
+				Y = myObj.nextFloat(); 
+				float Z = (X * ((100-Y)/100));
+				System.out.printf(
+						"Seu produto, após os descontos, tem o valor aproximado de %f.", Z);
+		 }
+}
+
+//sexta questão
