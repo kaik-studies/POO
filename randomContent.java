e7d7edb9358b3fbcc6badf6bf539450180612740
kaik-studies <164260383+kaik-studies@users.noreply.github.com>
2024-11-12 19:01:49 -0300

    Create randomContent.java

---
+++ randomContent.java
@@ -0,0 +1,17 @@
+package filename.java;
+
+public class filemame {
+  public static void main(String[] args) {
+int I = 3;
+System.out.println("Congrats, you'll receive a new message in: ");
+do {
+	System.out.println(I + "!");
+	I--;
+} while (I > 0); 
+if (I == 0) {
+	System.out.println("Happy Birthday, mate!");
+}
+  }
+}
+
+//random code execution
