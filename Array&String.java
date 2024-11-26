1033284bc45f28f043feb302c9bb591f16873a7c
kaik-studies <164260383+kaik-studies@users.noreply.github.com>
2024-11-26 20:00:42 -0300

    Update Array&String.java

--- Array&String.java
+++ Array&String.java
@@ -40,3 +40,49 @@ public class filenameclass {
 		}
 	}
 }
+
+//
+
+package atividadeUm;
+import java.util.Scanner;
+public class filenameclass {
+	public static void main(String[] args) {
+		System.out.println("Entre com o nome de 4 verduras: ");
+		Scanner myObj = new Scanner(System.in);
+		
+		int [] numeros = new int[4];
+
+		for (int I = 0; I <= 3; I++) {
+			numeros[I] = myObj.nextInt();
+		}
+		
+		for (int v: numeros) {
+			System.out.println();
+			System.out.println(v*2);
+		}
+				
+		}
+	}
+
+//teste
+
+package atividadeUm;
+import java.util.Scanner;
+public class filenameclass {
+	public static void main(String[] args) {
+
+		Scanner myObj = new Scanner(System.in);
+		
+		int [] num = { 1, 2, 3, 4, 5};
+		
+		int [] num_dobra = new int [5];
+		
+		for(int I = 0;I <= 4; I++) {
+			num_dobra[I] = num[I]*2;
+			System.out.print(num_dobra[I]);
+			System.out.print(",");
+		}
+		
+		}
+	
+	}
