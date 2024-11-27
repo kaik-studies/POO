7526d82357cf70a1d638861260f9214e21b4c06d
kaik-studies <164260383+kaik-studies@users.noreply.github.com>
2024-11-27 20:06:28 -0300

    Create aula.java

---
+++ aula.java
@@ -0,0 +1,48 @@
+package filename.java;
+import java.util.Scanner;
+public class pares {
+	
+	public static void main(String[] args) {
+		
+		Scanner myObj = new Scanner(System.in);
+		int fator = 1;
+		System.out.println("Digite um limite de fatorial");
+		int limite = myObj.nextInt();
+		for(int numeros = 1; numeros <= limite; numeros++) {
+			fator *= numeros;
+		
+			System.out.println(fator);
+			
+		}
+		//troque a chave de lugar para limitar os prints//
+	}
+
+}
+//quadrado
+
+package filename.java;
+
+public class cube {
+public static void main (String[] args) {
+	for(int A = 5; A <= 6; A++) {
+		
+		for(int B = 5; B <= 5; B++) {
+			System.out.println("******************");
+		}
+		for(int B = 5; B <= 5; B++) {
+			System.out.println("******************");
+		}
+		for(int B = 5; B <= 5; B++) {
+			System.out.println("******************");
+		}
+		for(int B = 5; B <= 5; B++) {
+			System.out.println("******************");
+		}
+		
+		
+	}
+}
+}
+
+//
+
