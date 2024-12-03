3510151f3d135277b69bb624631bdfcca09526d3
kaik-studies <164260383+kaik-studies@users.noreply.github.com>
2024-12-03 19:26:50 -0300

    Update aula.java

--- aula.java
+++ aula.java
@@ -45,4 +45,25 @@ public static void main (String[] args) {
 }
 
 //
+package filename.java;
+import java.util.Scanner;
+public class filemame
+
+{
+  public static void main(String[] args) {
+	  Scanner myObj = new Scanner(System.in);
+	  System.out.println("Nota da prova 1, da prova 2  da prova 3: ");
+	  double nota1 = myObj.nextDouble(), nota2 = myObj.nextDouble(), nota3 = myObj.nextDouble();
+	  if (nota1 > nota2 && nota1 > nota3) {
+		  System.out.println("Nota 1 é a maior nota.");
+		} else if (nota2 > nota1 && nota2 > nota3) {
+			System.out.println("Nota 2 é maior.");
+		} else if(nota3 > nota1 && nota3 > nota2){
+			System.out.println("Nota 3 é maior."); }
+			else {
+				System.out.println("Todas as notas são iguais.");
+			}
+		}
+
+  }
 
